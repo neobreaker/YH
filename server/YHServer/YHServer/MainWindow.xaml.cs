732fb8228df3cc3710e95e97d9bba89d90133ff2
Neo <769104314@qq.com>
2017-12-01 15:37:13 +0800

    work on server

--- server/YHServer/YHServer/MainWindow.xaml.cs
+++ server/YHServer/YHServer/MainWindow.xaml.cs
@@ -14,6 +14,7 @@ using System.Windows.Shapes;
 using System.Net;
 using System.Net.Sockets;
 using System.Threading;
+using YHServer.YHLib;
 
 namespace YHServer
 {
@@ -22,46 +23,16 @@ namespace YHServer
     /// </summary>
     public partial class MainWindow : Window
     {
-        private Socket m_rcvsocket = null;
-        private EndPoint m_remote_ep = null;
 
-        private Thread m_thread_rcv;
-      
-        private byte[] m_rcv_buffer;
-
+        private YHnet m_yhnet = null;
         public MainWindow()
         {
             InitializeComponent();
-            ListenSocketInit();
         }
 
         private void ListenSocketInit()
         {
-            // 初始化socket
-            m_rcvsocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
-            // 绑定本地端口
-            IPEndPoint local_ep = new IPEndPoint(IPAddress.Any, 50000);
-            m_rcvsocket.Bind(local_ep);
-
-            IPEndPoint sender = new IPEndPoint(IPAddress.Parse("192.168.3.60"), 50000);
-            m_remote_ep = (EndPoint)sender;
-
-            m_rcv_buffer = new Byte[4096];
-
-            m_thread_rcv = new Thread(ThreadDoRecv);
-            m_thread_rcv.IsBackground = true;
-            m_thread_rcv.Start();
-        }
-
-        private void ThreadDoRecv()
-        {
-            int cnt = 0;
-            while (true)
-            {
-                m_rcvsocket.ReceiveFrom(m_rcv_buffer, SocketFlags.None, ref m_remote_ep);
-
-                cnt++;
-            }
+            m_yhnet = new YHnet("192.168.3.60", 50000, 50000);
         }
 
         private void BtnConnect_Click(object sender, RoutedEventArgs e)
@@ -69,8 +40,7 @@ namespace YHServer
             byte[] data = new byte[1024];
             string cmd = "request";
             data = ASCIIEncoding.ASCII.GetBytes(cmd);
-
-            m_rcvsocket.SendTo(data, data.Length, SocketFlags.None, m_remote_ep);
+            m_yhnet.SendTo(data, data.Length);            
         }
     }
 }
