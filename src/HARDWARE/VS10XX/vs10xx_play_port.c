37ffff670d45decef52ac46dde50fe3b34aea389
Neo <769104314@qq.com>
2017-12-18 10:38:03 +0800

    fix vs10xx play dma

--- src/HARDWARE/VS10XX/vs10xx_play_port.c
+++ src/HARDWARE/VS10XX/vs10xx_play_port.c
@@ -100,12 +100,13 @@ void vs10xx_play_spi_init(void)
 
 }
 
-DMA_InitTypeDef DMA_InitStructure;
+
 void vs10xx_play_dma_init(u32 cpar, u32 cmar)
 {
+	DMA_InitTypeDef DMA_InitStructure;
 	RCC_AHBPeriphClockCmd(RCC_AHBPeriph_DMA2, ENABLE);	//使能DMA传输
 	
-	DMA_DeInit(DMA2_Channel2);   //将DMA的通道1寄存器重设为缺省值
+	DMA_DeInit(VS10XX_PLAY_DMA_CHx);   //将DMA的通道1寄存器重设为缺省值
 
 	DMA_InitStructure.DMA_PeripheralBaseAddr = cpar;  //DMA外设基地址
 	DMA_InitStructure.DMA_MemoryBaseAddr = cmar;  //DMA内存基地址
@@ -118,15 +119,15 @@ void vs10xx_play_dma_init(u32 cpar, u32 cmar)
 	DMA_InitStructure.DMA_Mode = DMA_Mode_Normal;  //工作在正常模式
 	DMA_InitStructure.DMA_Priority = DMA_Priority_Medium; //DMA通道 x拥有中优先级 
 	DMA_InitStructure.DMA_M2M = DMA_M2M_Disable;  //DMA通道x没有设置为内存到内存传输
-	DMA_Init(DMA_CHx, &DMA_InitStructure);  //根据DMA_InitStruct中指定的参数初始化DMA的通道USART1_Tx_DMA_Channel所标识的寄存器
+	DMA_Init(VS10XX_PLAY_DMA_CHx, &DMA_InitStructure);  //根据DMA_InitStruct中指定的参数初始化DMA的通道USART1_Tx_DMA_Channel所标识的寄存器
 }
 
 //开启一次DMA传输
 void vs10xx_play_dma_enable(u16 cndtr)
 { 
-	DMA_Cmd(DMA_CHx, DISABLE );  //关闭 所指示的通道      
- 	DMA_SetCurrDataCounter(DMA_CHx, cndtr);//DMA通道的DMA缓存的大小
- 	DMA_Cmd(DMA_CHx, ENABLE);  //使能 所指示的通道 
+	DMA_Cmd(VS10XX_PLAY_DMA_CHx, DISABLE );  //关闭 所指示的通道      
+ 	DMA_SetCurrDataCounter(VS10XX_PLAY_DMA_CHx, cndtr);//DMA通道的DMA缓存的大小
+ 	DMA_Cmd(VS10XX_PLAY_DMA_CHx, ENABLE);  //使能 所指示的通道 
 }	
 
 void vs10xx_play_init()
